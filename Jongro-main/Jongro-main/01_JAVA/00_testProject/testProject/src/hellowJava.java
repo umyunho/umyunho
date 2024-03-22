67c0e49760bea7f2a356c5d4ebe1137c34ca5566
DESKTOP-F4BC13L\hi <giantin48@gmail.com>
2024-03-22 18:13:59 +0900

    자바 연습

---
+++ Jongro-main/Jongro-main/01_JAVA/00_testProject/testProject/src/hellowJava.java
@@ -0,0 +1,7 @@
+public class hellowJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("즐겁게 수업합시다");
+    }
+}
---
+++ Jongro-main/Jongro-main/02_JAVA_pratice/chap02-operator-practice/src/com/ohgiraffers/level01/basic/Question.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.level01.basic;
+
+public class Question {
+
+    public static void main(String[] args) {
+
+        /* 예제 문제를 기재해주세요 */
+
+        /* Application01
+        int x = 2;
+		int y = 5;
+		char c = 'A'; // 'A'의 문자코드는 65
+		System.out.println(y >= 5 || x < 0 && x > 2);
+		System.out.println(y += 10 - x++);
+		System.out.println(x+=2);
+		System.out.println( !('A' <= c && c <='Z') );
+		System.out.println('C'-c);
+		System.out.println('5'-'0');
+		System.out.println(c+1);
+		System.out.println(++c);
+		System.out.println(c++);
+		System.out.println(c);
+
+		System.out.println() 의 결과를 예측하고 이유를 설명하시오.
+		*/
+
+        /* Application02
+         *
+         * 내가 가지고 있는 사과의 갯수는 92개이다.
+         * 이를 담을 수 있는 바구니에는 10개의 사과를 담을 수 있다면 총 10개의 바구니가 필요할 것이다.
+         * 아래에 알맞은 코드를 넣으시오.
+         *
+         * int numOfApples = 92;
+         * int sizeOfBucket = 10;
+         * int numOfBucket = ?
+         *
+         * System.out.println("필요한 바구니의 수 : " + numOfBucket); -> 10
+         */
+
+    }
+
+}
